dd229de015ddec9fc7b9a06f431fa65f39ebd48e
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-08-11 14:55:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title('Manfacturing Classification System')  # 타이틀명 지정
 st.write('Set API Name and Conent(%) to Get Formulation Recommendation')
 link1 = '[Formulation Recommendation](http://mcs-jaeuk-recommendation.streamlit.app)'
 st.markdown(link1, unsafe_allow_html=True)
-
+st.write("")
 st.write('Design Your Formulation and Check Manufacturing Class')
 link2 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link2, unsafe_allow_html=True)
