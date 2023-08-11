4f38ae8913305ca526d89b6df8958b72550d6be1
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-08-11 14:56:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ st.write('Set API Name and Conent(%) to Get Formulation Recommendation')
 link1 = '[Formulation Recommendation](http://mcs-jaeuk-recommendation.streamlit.app)'
 st.markdown(link1, unsafe_allow_html=True)
 st.write("")
+st.write("")
 st.write('Design Your Formulation and Check Manufacturing Class')
 link2 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link2, unsafe_allow_html=True)
