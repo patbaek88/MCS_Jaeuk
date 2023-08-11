053ebd2bf065311f1dec20f8ab1f057292c2f7ea
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-08-11 14:34:58 +0900

    Update formulation_recommendation.py

--- formulation_recommendation.py
+++ formulation_recommendation.py
@@ -292,7 +292,7 @@ if st.button("Recommend"):
     st.write("Recommended Formulation = " +Recommanded_Formulation_Name)
     st.write("Mixture MCS result = "+MCS_result)
     st.write("---------------------Recommended Formulation Detail---------------------")
-    st.write("API name / PC1 score / content = "+API_name+" / "+API_PC1_score_1+" / "+API_content_f+" %")
+    st.write("API name / PC1 score / content = "+API_name+" / "+API_PC1_score_1+" / "+API_content+" %")
     st.write("Excipient1 name / PC1 score / content = "+Excipient1_name+" / "+Excipient1_PC1_score_1+" / "+Excipient1_content_1+" % ")
     st.write("Excipient2 name / PC1 score / content = "+Excipient2_name+" / "+Excipient2_PC1_score_1+" / "+Excipient2_content_1+" % ")
     st.write("Excipient3 name / PC1 score / content = "+Excipient3_name+" / "+Excipient3_PC1_score_1+" / "+Excipient3_content_1+" % ")
