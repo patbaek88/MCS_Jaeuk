54b52dc94318be66a4eb5f280edd23f7d9eb34e4
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-08-11 11:52:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ pca = PCA(n_components=4)  # 주성분을 몇개로 할지 결정
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data=principalComponents, index=df.index, columns=['pc1', 'pc2', 'pc3', 'pc4'])
 # 주성분으로 이루어진 데이터 프레임 구성
+st.write(pca.explained_variance_ratio_)
 
 principalDf["Classification"] = df['Classification']
 principalDf_WG = principalDf[(principalDf['Classification'] == 'Filler_WG')]
