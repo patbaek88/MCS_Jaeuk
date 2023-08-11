b381452edab1491d99334f2b084bd84289a911af
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-08-11 14:23:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import streamlit as st
+
 st.title('Manfacturing Classification System')  # 타이틀명 지정
 
-
 st.write('Design Formulations and Check Manufacturing Class')
-link1 = '[GitHub](http://mcs-jaeuk-design.streamlit.app)'
+link1 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link1, unsafe_allow_html=True)
 
