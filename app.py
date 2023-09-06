a121dd208e48955a919743371e7840db2ff1011d
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-09-07 08:27:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ link2 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link2, unsafe_allow_html=True)
 st.write("")
 st.write("")
-st.write('Current Data Base')
+st.write('Current FT4 Data Base')
 database = 'Excipients_APIs_DB_Feb2023.csv'
 df_database = pd.read_csv(database)
 st.write(df_database)
+st.write('FT4 Features')
 ft4_features = 'FT4_features.csv'
 df_ft4_features = pd.read_csv(ft4_features)
 st.write(df_ft4_features)
