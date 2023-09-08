c7a7249c806fba9e57ab3c1fe3acfbeed6223592
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-09-08 13:56:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,23 +14,18 @@ st.markdown(link2, unsafe_allow_html=True)
 st.write("")
 st.write("")
 
-#list_of_names = ['FT4_DB_Feb2023','FT4_features']
-#dataframes_list = []
-#for i in range(len(list_of_names)):
-#    temp_df = pd.read_csv(list_of_names[i]+".csv")
-#    dataframes_list.append(temp_df)
 
-multiple_files = st.file_uploader('CSV',type="csv", accept_multiple_files=True)
-for file in multiple_files:
-	dataframe = pd.read_csv(file)
-	file.seek(0)
-	st.write(dataframe)
+#multiple_files = st.file_uploader('CSV',type="csv", accept_multiple_files=True)
+#for file in multiple_files:
+#	dataframe = pd.read_csv(file)
+#	file.seek(0)
+#	st.write(dataframe)
 
-#database = 'FT4_DB_Feb2023.csv'
-#df_database = pd.read_csv(database)
-#ft4_features = 'FT4_features.csv'
-#df_ft4_features = pd.read_csv(ft4_features)
-#st.write('FT4 Data Base ('+database+')')
-#st.write(dataframes_list[0])
+database = 'FT4_DB_Feb2023.csv'
+df_database = pd.read_csv(database)
+ft4_features = 'FT4_features.csv'
+df_ft4_features = pd.read_csv(ft4_features)
+st.write('FT4 Data Base ('+database+')')
+st.write(df_database)
 st.write('FT4 Features')
-#st.write(dataframes_list[1])
+st.write(df_ft4_features)
