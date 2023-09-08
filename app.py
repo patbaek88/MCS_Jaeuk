57b310641439d912594e5dd16ce39e35952b67e0
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-09-08 12:39:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ import pandas as pd
 
 st.title('Manfacturing Classification System')  # 타이틀명 지정
 
-st.write('Formulation Recommendation')
 link1 = '[Formulation Recommendation](http://mcs-jaeuk-recommendation.streamlit.app)'
 st.markdown(link1, unsafe_allow_html=True)
 st.write("")
 st.write("")
-st.write('Formulation Design')
 link2 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link2, unsafe_allow_html=True)
 st.write("")
