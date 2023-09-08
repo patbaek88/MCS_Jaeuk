6fc57775139126ddccfae42cf0ad686ca22d1920
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2023-09-08 13:27:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,18 @@ link2 = '[Formulation Design](http://mcs-jaeuk-design.streamlit.app)'
 st.markdown(link2, unsafe_allow_html=True)
 st.write("")
 st.write("")
-database = 'FT4_DB_Feb2023.csv'
-df_database = pd.read_csv(database)
-ft4_features = 'FT4_features.csv'
-df_ft4_features = pd.read_csv(ft4_features)
+
+list_of_names = ['FT4_DB_Feb2023','FT4_features']
+dataframes_list = []
+for i in range(len(list_of_names)):
+    temp_df = pd.read_csv("./csv/"+list_of_names[i]+".csv")
+    dataframes_list.append(temp_df)
+
+#database = 'FT4_DB_Feb2023.csv'
+#df_database = pd.read_csv(database)
+#ft4_features = 'FT4_features.csv'
+#df_ft4_features = pd.read_csv(ft4_features)
 st.write('FT4 Data Base ('+database+')')
-st.write(df_database)
+st.write(dataframes_list[0])
 st.write('FT4 Features')
-#st.write(df_ft4_features)
+#st.write(dataframes_list[1])
