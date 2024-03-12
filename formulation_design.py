3f129ddd156ea555b96b75e10d8d1d436bd259e6
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-03-12 14:16:54 +0900

    Update formulation_design.py

--- formulation_design.py
+++ formulation_design.py
@@ -69,6 +69,7 @@ critical_value_class4_1 = str(round(critical_value_class4, 3))
 
 df_pc1_score = pd.DataFrame(data=df, index=df.index, columns=['Function','PC1_score'])
 
+st.write(principalDf)                                                                  
 st.write(df_pc1_score)
 st.write("Class 1 : Direct Compression,   PC1 score < " + critical_value_class1_1)
 st.write("Class 2 : Dry Granulation,        PC1 score < " + critical_value_class3_1)
